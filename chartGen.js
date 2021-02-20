008b071186775f0aa476f59396f70cdb83e8bb7c
Benjamin Perez <brperez4@CougarNet.UH.EDU>
2021-02-20 16:00:15 -0600

    First attempt at implementing d3

---
+++ chartGen.js
@@ -0,0 +1,35 @@
+
+var megUsesCsvPath = "Asset/MEG Uses.csv";
+
+var megChart = d3.select("#meg-bar-chart");
+
+var margin = 50;
+var width = megChart.attr("width") - margin;
+var height = megChart.attr("height") - margin;
+
+var xScale = d3.scaleBand().range([0,width]).padding(0.4);
+var yScale = d3.scaleLinear().range([height,0]);
+
+d3.csv(megUsesCsvPath, function(err, data) {
+    
+    if(err){
+        throw err;
+    }
+
+    xScale.domain(data.map(function(d){return d.Application;}));
+    yScale.domain([0, d3.max(data, function(d) { return d.Amount;})])
+
+    g = megChart.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(xScale));
+
+    g.append("g")
+        .call(d3.axisLeft(yScale).tickFormat(function(d){
+            return d;
+        }).ticks(10))
+        .append("text")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("value");
+})
