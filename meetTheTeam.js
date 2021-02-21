63d0ee4a237ecde03e12ad818db0eefdb0a08c25
Benjamin Perez <brperez4@CougarNet.UH.EDU>
2021-02-20 22:49:54 -0600

    Implementing profile cards

--- meetTheTeam.js
+++ meetTheTeam.js
@@ -3,19 +3,39 @@
 var teamSection = d3.select("#team-section");
 var memberDutiesJsonPath = "Assets/MemberDuties.json";
 
+/* <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div> */
+
 d3.json(memberDutiesJsonPath, d => d)
   .then(data => {
-    var divs = teamSection.selectAll("div")
-                            .data(data)
-                            .enter()
-                            .append("div")
-                            .attr("class","column");
+    var profileCards = teamSection.selectAll("div")
+                                    .data(data)
+                                    .enter()
+                                    .append("div")
+                                    .attr("class","card")
+                                    .attr("style","width: 20rem")
+                                    .append("div")
+                                    .attr("class","card-body");
 
-    divs.append("h6").text(d => d.Member);
-    divs.append("p").text(d => d.Duties);
-    divs.append("a").attr("href", d => d.LinkedIn)
-                    .attr("target", "_blank")
-                    .text("LinkedIn");
+    profileCards.append("h5")
+                .attr("class","card-title")
+                .text(d => d.Member);
+
+    profileCards.append("p")
+                .attr("class","card-text")
+                .text(d => d.Duties);
+
+    profileCards.append("a")
+                .attr("href",d => d.LinkedIn)
+                .text("LinkedIn");
+
     // teamSection.selectAll("div")
     //            .data(data)
     //            .enter()
